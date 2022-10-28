9d40af59cdb126009f7ec19a34e010f1599adb6e
Pedro <pdegaudenci@hotmail.com>
2022-10-28 15:02:35 +0200

    ACtualizado product_Service

--- router/products.py
+++ router/products.py
@@ -13,6 +13,7 @@ from config.db_config import session
 products_router = APIRouter()
 
 
+
 @products_router.get('/products')
 def get_products():
     result = session.query(Product).all()
@@ -40,26 +41,12 @@ def delete_product(sku : int ):
     return result
 
 ## CREACION DE PRODUCTOS
-@products_router.post(path="/product",
-    status_code=status.HTTP_201_CREATED ,
-    summary="Create a new product",
-    tags=["Products"])
+@products_router.post(path="/product",status_code=status.HTTP_201_CREATED ,summary="Create a new product", tags=["Products"])
 def create_product(product:Product_create):
-    respuesta = None
-    producto = products_service.validate_product_FKs(product)
-    if producto != None:
-        # Ejecuto transaccion para agregar el producto
-        try:
-            # Agregar producto
-            session.add(producto)
-            #Ejecutar transaccion (Podria ejecutar previamente varias operaciones y al hacer commit se ejecutarian como una sola unidad sobre la base de datos)
-            session.commit()
-            session.refresh(producto)
-            respuesta = JSONResponse(content={"status_code":status.HTTP_201_CREATED, "data":jsonable_encoder(product)})
-        except: 
-            # En caso que no se pueda ejecutar la insercion, hago rollback de la trasaccion y lanzo un HttpException
-            session.rollback()
-            raise HTTPException(404, detail='Transaction Error product')    
+    # Verificacion de id de categoria,status o memoria sean FKs validos
+    result = products_service.validate_product_FKs(product,"create")
+    if result:
+         respuesta=products_service.create_product(product)  
     else:
         raise HTTPException(404, detail='category or status or memory not found by id')    
     return respuesta
@@ -76,11 +63,9 @@ def update_product(sku:int, product: Product_update):
             #Si valor es nulo elimino clave del diccionario --> Valor None significa que ese campo no sea ha enviado para actualizar
             values.pop(key)
     #  Actualizo valores de campos del registro con una query
-    if "category" or "status" or "memory" in values:
-            products_service.validate_fk(Categories.id_category,product.category,Categories)
-            products_service.validate_fk(Status.id_status,product.status,Status)
-            products_service.validate_fk(Memory.id_memory,product.memory,Memory)
-        
-    session.query(Product).filter(Product.sku == sku).update(values, synchronize_session='fetch')
-    session.commit()
-    return product
+    result = products_service.validate_product_FKs(product,"update")
+    if result:
+        products_service.update_product(product,sku,values)
+    else:
+        raise HTTPException(404, detail='category or status or memory id error')    
+    return session.query(Product).filter(Product.sku == sku).first()
--- services/products_service.py
+++ services/products_service.py
@@ -1,22 +1,59 @@
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi import HTTPException, status
+
 from config.db_config import session
 from models.models import Product, Status, Memory,Categories
 
 
-def validate_product_FKs(product, operation):
+def validate_product_FKs(product,operation):
     result = None
-    #Verificar si los id de categoria, status y memory existen en sus respectivas tablas 
-    busqueda = validate_fk(Categories,Categories.category_id,product.category)!=None and validate_fk(Status,Status.id_status,product.status) != None and  validate_fk(Memory,Memory.id_memory, product.memory) != None
-    if busqueda:
-        # Obtengo ultimo registro insertado en la tabla --> obtengo sku de ese producto y le sumo 1 unidad, lo que nos resultara en el nuevo sku 
-        # del producto a crear
-        obj = session.query(Product).order_by(Product.sku.desc()).first()
-        producto = Product(obj.sku +1,product.name,product.price,product.description,product.track_iventory,product.qty,product.weight,product.height,product.width,product.length,
-                        product.image_url,product.seo_title,product.seo_desc,product.color,product.status,product.category,product.memory)
-        result = producto
-    
+    #Verificar si los id de categoria, status y memory existen en sus respectivas tablas
+    if operation == "create":
+        result = validate_fk(Categories,Categories.id_category,product.category)!=None and validate_fk(Status,Status.id_status,product.status) != None and  validate_fk(Memory,Memory.id_memory, product.memory) != None
+    else:
+        if product.category !=None:
+            result = validate_fk(Categories,Categories.id_category,product.category)
+        if product.status != None:
+            result = validate_fk(Status,Status.id_status,product.status)
+        if product.memory != None:
+            result = validate_fk(Memory,Memory.id_memory, product.memory)
     return result
 
-def validate_fk(fk,fk_request, table):
+def create_product(product):
+     # Obtengo ultimo registro insertado en la tabla --> obtengo sku de ese producto y le sumo 1 unidad, lo que nos resultara en el nuevo sku 
+     # del producto a crear
+    respuesta = None
+    obj = session.query(Product).order_by(Product.sku.desc()).first()
+    producto = Product(obj.sku +1,product.name,product.price,product.description,product.track_iventory,product.qty,product.weight,product.height,product.width,product.length,
+                        product.image_url,product.seo_title,product.seo_desc,product.color,product.status,product.category,product.memory)
+     # Ejecuto transaccion para agregar el producto
+    try:
+        # Agregar producto
+        session.add(producto)
+        #Ejecutar transaccion (Podria ejecutar previamente varias operaciones y al hacer commit se ejecutarian como una sola unidad sobre la base de datos)
+        session.commit()
+        session.refresh(producto)
+        respuesta = JSONResponse(content={"status_code":status.HTTP_201_CREATED, "data":jsonable_encoder(product)})
+    except: 
+            # En caso que no se pueda ejecutar la insercion, hago rollback de la trasaccion y lanzo un HttpException
+            session.rollback()
+            raise HTTPException(404, detail='Transaction Error product')
+    return respuesta
+
+
+def validate_fk(table,fk,fk_request):
     return session.query(table).filter(fk == fk_request).first()
+
+def update_product(product,sku,values):
+    try:
+        session.query(Product).filter(Product.sku == sku).update(values, synchronize_session='fetch')
+        session.commit()
+        session.refresh(product)
+        respuesta = JSONResponse(content={"status_code":status.HTTP_202_ACCEPTED, "data":jsonable_encoder(product)})
+    except: 
+            session.rollback()
+            raise HTTPException(404, detail='Product Transaction Error ')
+    return respuesta
 
     
